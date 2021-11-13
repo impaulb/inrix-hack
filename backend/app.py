42f1b16e6514f8de987f08264625449d35359b4f
Tim Hradil <timhradil@gmail.com>
2021-11-13 11:40:33 -0800

    Added .env file

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
-appId = "xznhrx39ag"
-appKey = "TSuX6rbGPR1JCLTgHWETX9TvEztAgzU04Bhy7fxT"
-hashToken = "eHpuaHJ4MzlhZ3xUU3VYNnJiR1BSMUpDTFRnSFdFVFg5VHZFenRBZ3pVMDRCaHk3ZnhU"
-expirationDate = "2021-11-28T18:46:48.65Z"
+appId = os.getenv("appID")
+appKey = os.getenv("appKey")
+hashToken = os.getenv("hashToken")
+expirationDate = os.getenv("expeirationDate")
 
 # Get risk for a route using start and end point
 @app.route('/risk', methods=['GET'])

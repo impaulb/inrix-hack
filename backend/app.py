5af2142dae05946ed49d2c64e6b56840f919b74b
Paul <59670248+impaulb@users.noreply.github.com>
2021-11-13 11:24:03 -0800

    Basic parameters added

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import requests
+import json
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +10,8 @@ appKey = "TSuX6rbGPR1JCLTgHWETX9TvEztAgzU04Bhy7fxT"
 hashToken = "eHpuaHJ4MzlhZ3xUU3VYNnJiR1BSMUpDTFRnSFdFVFg5VHZFenRBZ3pVMDRCaHk3ZnhU"
 expirationDate = "2021-11-28T18:46:48.65Z"
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Get risk for a route using start and end point
+@app.route('/risk', methods=['GET'])
+def risk():
+    wp_1 = request.args.get('wp_1', default = 'ERR', type = str)
+    return wp_1

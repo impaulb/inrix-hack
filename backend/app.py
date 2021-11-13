43bb2c68331bcd59972b357d20b2f868521ecf26
Tim Hradil <timhradil@gmail.com>
2021-11-13 12:30:31 -0800

    Set up helpers.py and made boundingBoxToString

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import json
 import requests
 import os
+from helpers import *
 
 load_dotenv()
 
@@ -14,10 +15,15 @@ APP_KEY = os.getenv('APP_KEY')
 HASH_TOKEN = os.getenv('HASH_TOKEN')
 EXPIRATION_DATE = os.getenv('EXPIRATION_DATE')
 
+BASE_URL = os.getenv('BASE_URL')
+
 # Get risk for a route using start and end point
 @app.route('/risk', methods=['GET'])
 def risk():
     wp_1 = request.args.get('wp_1', default = '37.7958,-122.3938', type = str)
     wp_2 = request.args.get('wp_2', default = '37.7212,-122.4919', type = str)
-    
+
+    # Turn boudingBox json into boundingBoxString
+    boundingBoxString = boundingBoxToString(boundingBox)
+
     return wp_1
